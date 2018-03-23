5e5cd0508407506933de02ec7cf4391256ec2c6e
igorklipa <igorklipa@yahoo.com>
2018-03-23 15:57:04 +0100

    Init commit

---
+++ app/src/main/java/com/example/iklipa/formstepsview/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.iklipa.formstepsview;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import butterknife.BindView;
+import butterknife.Unbinder;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.headerFragment)
+    Fragment headerFragment;
+    @BindView(R.id.contentFragment)
+    Fragment contentFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initHeaderFragment();
+    }
+
+    private void initHeaderFragment() {
+
+    }
+
+}
---
+++ app/src/main/java/com/example/iklipa/formstepsview/fragments/FormContentFragment.java
@@ -0,0 +1,10 @@
+package com.example.iklipa.formstepsview.fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by iklipa on 3/23/2018.
+ */
+
+public class FormContentFragment extends Fragment {
+}
